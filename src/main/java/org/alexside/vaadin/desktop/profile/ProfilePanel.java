92a8d925cc23cb0db4d345161354a5dcd1d771b9
Alex <alexbalu-alpha7@mail.ru>
2016-12-17 17:34:27 +0300

    added image upload and scaling

--- src/main/java/org/alexside/vaadin/desktop/profile/ProfilePanel.java
+++ src/main/java/org/alexside/vaadin/desktop/profile/ProfilePanel.java
@@ -1,14 +1,22 @@
 package org.alexside.vaadin.desktop.profile;
 
+import com.vaadin.server.FileResource;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.*;
 import org.alexside.entity.User;
 import org.alexside.utils.MailUtils;
+import org.imgscalr.Scalr;
 
 import javax.annotation.PostConstruct;
-
-import static org.alexside.utils.ThemeUtils.PANEL_SCROLLABLE;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by Alex on 11.12.2016.
@@ -24,6 +32,7 @@ public class ProfilePanel extends Window{
     private TextField emailField;
     private Upload avatarUpload;
     private User user;
+    private FileUploader fileUploader;
 
     public ProfilePanel() {
         setWidth("70%");
@@ -47,13 +56,14 @@ public class ProfilePanel extends Window{
         emailField = new TextField("Почта");
         emailField.setSizeFull();
 
-        avatarUpload = new Upload();
+        fileUploader = new FileUploader();
+
+        avatarUpload = new Upload("Аватарка", fileUploader);
         avatarUpload.setSizeFull();
-        avatarUpload.setCaption("Аватарка");
+        avatarUpload.addSucceededListener(fileUploader);
 
         FormLayout commonWrap = new FormLayout(loginField, passCheckbox,
-                passFieldOne, passFieldTwo, emailField, avatarUpload);
-        commonWrap.setSizeFull();
+                passFieldOne, passFieldTwo, emailField, avatarUpload, fileUploader.getImage());
 
         Button mailButton = new Button("Отправить");
         mailButton.addClickListener(event -> {
@@ -62,12 +72,12 @@ public class ProfilePanel extends Window{
         commonWrap.addComponentAsFirst(mailButton);
 
         TabSheet tabSheet = new TabSheet();
+        tabSheet.setSizeFull();
         tabSheet.addTab(commonWrap, user != null ? "Общие" : "Регистрация");
 
         VerticalLayout wrap = new VerticalLayout(tabSheet);
         wrap.setSizeFull();
         wrap.setMargin(true);
-        wrap.addStyleName(PANEL_SCROLLABLE);
         setContent(wrap);
     }
 
@@ -91,5 +101,48 @@ public class ProfilePanel extends Window{
         passFieldOne.clear();
         passFieldTwo.clear();
         emailField.clear();
+    }
+
+    public static class FileUploader implements Upload.Receiver, Upload.SucceededListener {
+        private File file;
+        private final Embedded image;
+
+        public FileUploader() {
+            image = new Embedded("Uploaded Image");
+            image.setHeight("80px");
+            image.setWidthUndefined();
+            image.setVisible(false);
+            image.addStyleName("avatar_panel");
+        }
+
+        public OutputStream receiveUpload(String filename,
+                                          String mimeType) {
+            FileOutputStream fos;
+            try {
+                String path = VaadinServlet.getCurrent().getServletContext().getRealPath("/VAADIN/tmp/uploads/");
+                file = new File(path + filename);
+                fos = new FileOutputStream(file);
+            } catch (IOException e) {
+                new Notification("Could not open file<br/>",  e.getMessage(),
+                        Notification.Type.ERROR_MESSAGE).show(Page.getCurrent());
+                return null;
+            }
+            return fos; // Return the output stream to write to
+        }
+
+        public void uploadSucceeded(Upload.SucceededEvent event) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(file);
+                BufferedImage scaledImage = Scalr.resize(bufferedImage, 200);
+                ImageIO.write(scaledImage, "png", file);
+            } catch (IOException e) {
+                new Notification("Could not resize file<br/>",  e.getMessage(),
+                        Notification.Type.ERROR_MESSAGE).show(Page.getCurrent());
+            }
+            image.setSource(new FileResource(file));
+            image.setVisible(true);
+        }
+
+        public Embedded getImage() { return image; }
     }
 }
